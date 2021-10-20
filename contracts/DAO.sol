c07a023cd32c245e0ac20f412c95dbf68c96ae25
Tamar Jordania <rysshe>
2021-10-20 18:39:32 +0300

    Change the structure

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "./Pool.sol"
+import "./DAOToken.sol"
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
@@ -10,7 +11,7 @@ contract DAO is Initializable {
 
     // три состояния даю
     // cбор, сбор закончен, сбор отменен
-    Pool pool = new Pool();
+    Pool pool;
     uint goal;
     IERC20Upgradeable dao_token;
 
@@ -21,25 +22,23 @@ contract DAO is Initializable {
     // Every time DAO is created, 
     tages public stage = Stages.IN_PROGRESS;
 
-    function initialize (string memory _name,
-                IERC20Upgradeable _dao_token,
-                IERC721Upgradeable _nftAddress,
+    function initialize (IERC721Upgradeable _nftAddress,
                 uint _nftId, 
-                uint payment, // to create DAO you need to send the first payment
-                uint goal,
-                address creator,
-                string memory _partyName, 
-                string memory _ticker) public initializer {
+                uint _payment, // to create DAO you need to send the first payment
+                uint _goal,
+                address _creator,
+                string memory _name, 
+                string memory _ticker,
+                uint _daoId) internal initializer {
         
-        pool.initialize()
+        pool.initialize(_goal, _payment, _creator)
+        //
+        dao_token = new DAOToken(_name, _ticker, _shares_amount, address(pool));
         name = _name;
-        dao_token = _dao_token;
+        
 
     function recieve_money(address _user) public payable {
 
     }
-
-
-        
     }
 }
