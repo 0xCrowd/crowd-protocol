3f94d15f7bdbcfead8c9b5d9b6e98d3e117b2171
Tamar Jordania <rysshe>
2021-10-21 19:11:53 +0300

    Add deploy

--- migrations/2_deploy_upgradable_pool.js
+++ migrations/2_deploy_upgradable_pool.js
@@ -1,9 +1,16 @@
-const Pool = artifacts.require("v1Pool");
 const CRUD = artifacts.require("CRUD");
+const DAO = artifacts.require("DAO");
+const DAOFactory = artifacts.require("DAOFactory");
+const DAOToken = artifacts.require("DAOToken");
+
 const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
 module.exports = async function (deployer) {
    await deployer.deploy(CRUD);
-   await deployProxy(Pool, "0x891877E6d4047d8e397BF7ed5F12DCe8BAda212f", { deployer, initializer: 'initialize' });
-   console.log("Pool:", (await Pool.deployed()).address);
+   await deployer.deploy(DAOFactory, ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4"], { deployer, initializer: 'initialize' });
+   await deployer.deploy(DAO);
+   await deployer.deploy(DAOToken);
+
+   console.log("Factory:", (await DAOFactory.deployed()).address);
+   console.log("DAO:", (await DAO.deployed()).address);
 };
