7bb9e7353d202d7ffdb061b393331a1dbbadb2d4
Tamar Jordania <rysshe>
2021-10-29 18:25:08 +0300

    artifacts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,3 @@
-//import { assert } from "console";
 import hre from "hardhat";
 
 
@@ -17,20 +16,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-
-
-
-/*
-before("Get Factory", async function () {
-  this.Factory = await hre.ethers.getContractFactory("DAOFactory");
-});
-
-it("Deploys", async function () {
-  const deployedFactory = await hre.upgrades.deployProxy(this.Factory,
-    ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"],
-    { initializer: 'initialize' });
-
-  assert(await deployedFactory.owner === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-  
-});
-*/
