71b569d5c200ee885f7c397d0ddd672990268a79
Tamar Jordania <rysshe>
2021-10-29 13:49:00 +0300

    Remove truffle

--- migrations/2_deploy_pool.js
+++
@@ -1,7 +0,0 @@
-const DAOFactory = artifacts.require("DAOFactory");
-
-module.exports = async function (deployer) {
-   await deployer.deploy(DAOFactory, "0x8b88b18F79A4548738009fe6817BDB375D1e437b", "0x000582E365fEAcd919C1FFaAfF2616476eDf64c7");
-   const dao_factory = await DAOFactory.deployed();
-   console.log("Factory:", dao_factory.address);
-};
