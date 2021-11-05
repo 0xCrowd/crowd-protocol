b063c0991fb995690f1ea25a74c053afab2016d6
Tamar Jordania <rysshe>
2021-11-05 17:38:46 +0300

    Add comments

--- test/tests.js
+++ test/tests.js
@@ -9,8 +9,9 @@ describe("Test Factory...", function(){
   const tokenName = "GOGA";
   const tokenTicker = "GO";
   before("Deploy factory proxy...", async function () {
-    // Get addresses.
+    // Get Factory contract.
     this.Factory = await hre.ethers.getContractFactory("Factory");
+    // Get owner address.
     [owner, ...addrs] = await hre.ethers.getSigners();
     // Deploy updradable Factory.
     factory = await hre.upgrades.deployProxy(this.Factory, [owner.address], 
@@ -32,12 +33,14 @@ describe("Test Factory...", function(){
     let vault;
     before("Create a new Vault with the first deposit == 0.3 ETH...", async function () {
       await factory.newVault(tokenName, tokenTicker, 10, { value: hre.ethers.utils.parseEther("0.3")});
-      // The first Vault has index 0.
+      // Vaults are stored in dynamic array by their serial number, starting with 0.
+      // Get the address of the first Vault using the zero index.
       vaultAddr = await factory.getVaultAddress(0);
       // Get the Vault.
       const Vault = await hre.ethers.getContractFactory("Vault");
       vault = await Vault.attach(vaultAddr);
       console.log("Vault initiator before setup:", await vault.initiator())
+      // Setup vault initiator using factory as the factory contract if the owner of tha Vault.
       await factory.setupVault(vaultAddr, owner.address);
       console.log("Vault initiator after setup:", await vault.initiator())
       expect(await vault.initiator()).to.equal(owner.address);
@@ -50,6 +53,7 @@ describe("Test Factory...", function(){
     });
     it("Check switch stage to FULL...", async function () {
       await vault.goFullStage();
+      // 0 stage - in progress, 1 stage - FULL.
       expect(await vault.stage()).to.equal(1);
     });
     it("Check tokens distribution...", async function (){
